271655bb4a2d0a8f3181e625997be53bc65dcb5f
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-05-31 12:06:35 +0100

    fix: edit tab buttons

--- components/TabbedButtons.tsx
+++ components/TabbedButtons.tsx
@@ -4,27 +4,39 @@ type ChartTabButtons = {
   options: string[];
 };
 
-export default function TabedButtons({ options }: ChartTabButtons) {
+export default function ChartTabButtons({ options }: ChartTabButtons) {
   const [selected, setSelected] = useState<string>(options[0]);
 
   return (
-    <div className="flex rounded-lg border border-gray-200 bg-white shadow-sm transition-colors max-w-fit">
-      {options.map((option, index) => (
-        <button
-          key={index}
-          onClick={() => setSelected(option)}
-          className={`text-[#1D2939] flex items-center px-4 py-2 focus:outline-none transition-colors ${
-            selected === option ? "bg-[#ebedf0]" : "bg-white hover:bg-[#D0D5DD]"
-          }`}
-        >
-          {/* <span
-            className={`mr-2 inline-block h-2 w-2 rounded-full ${
-              selected === option ? "bg-[#0052CC]" : "bg-[#B692F6]"
+    <div className="inline-flex rounded-lg border border-gray-200 bg-white shadow-sm max-w-fit">
+      {options.map((option, index) => {
+        const isFirst = index === 0;
+        const isLast = index === options.length - 1;
+        const isThird = index === 2; // Assuming third button has index 2
+
+        return (
+          <button
+            key={index}
+            onClick={() => setSelected(option)}
+            className={`text-[#1D2939] flex items-center px-4 py-2 focus:outline-none transition-colors ${
+              selected === option
+                ? "bg-[#ebedf0]"
+                : "bg-white hover:bg-gray-100"
+            } ${isFirst ? "rounded-l-md" : ""} ${
+              isLast ? "rounded-r-md" : ""
+            } ${!isFirst && !isLast ? "border-l border-gray-200" : ""} ${
+              isThird ? "border-r border-gray-200" : ""
             }`}
-          ></span> */}
-          {option}
-        </button>
-      ))}
+          >
+            {/* <span
+              className={`mr-2 inline-block h-2 w-2 rounded-full ${
+                selected === option ? "bg-[#0052CC]" : "bg-[#B692F6]"
+              }`}
+            ></span> */}
+            {option}
+          </button>
+        );
+      })}
     </div>
   );
 }
